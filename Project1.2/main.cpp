171e702cfc9423c7ae7b200a260b23d14f70f38e
xiacheyun <Xcy@xiacheyundeMacBook-Pro.local>
2015-01-10 16:08:35 -0800

    Initial Commit

---
+++ Project1.2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project1.2
+//
+//  Created by xiacheyun on 1/10/15.
+//  Copyright (c) 2015 xiacheyun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
